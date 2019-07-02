02bfa383ffe5ff0b180c929690111af40f706eea
sundowns <tom@smallridge.com.au>
2019-07-02 22:23:54 +0800

    test love-release on CI

--- conf.lua
+++ conf.lua
@@ -3,4 +3,18 @@ function love.conf(t)
     t.window.minwidth = 1280
     t.window.minheight = 720
     t.console = true
+
+    t.releases = {
+        title = "Grim Gamers", -- The project title (string)
+        package = nil, -- The project command and package name (string)
+        loveVersion = nil, -- The project LÖVE version
+        version = nil, -- The project version
+        author = "Tom Smallridge", -- Your name (string)
+        email = nil, -- Your email (string)
+        description = nil, -- The project description (string)
+        homepage = nil, -- The project homepage (string)
+        identifier = nil, -- The project Uniform Type Identifier (string)
+        excludeFileList = {".git", "tests", ".luacheckrc", "README.md", ".vscode", ".circleci", ".gitignore"},
+        releaseDirectory = nil -- Where to store the project releases (string)
+    }
 end
