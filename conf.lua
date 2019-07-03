1b87dfb42bd0cee16a1e46b6843fafbae8f1889e
Tom Smallridge <tom@smallridge.com.au>
2019-07-03 11:37:44 +0800

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -14,7 +14,7 @@ function love.conf(t)
         description = "we gamer", -- The project description (string)
         homepage = "https://example.com", -- The project homepage (string)
         identifier = "grim.gamers", -- The project Uniform Type Identifier (string)
-        excludeFileList = {".git", "tests", ".luacheckrc", "README.md", ".vscode", ".circleci", ".gitignore"},
+        excludeFileList = {".git", "tests", ".luacheckrc", "README.md", ".vscode", ".circleci", ".gitignore", "tmp", "*.tmx"},
         releaseDirectory = "dist" -- Where to store the project releases (string)
     }
 end
