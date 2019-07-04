dc15a7ad6669b4ff2eecd713f705aeb9d144da76
Sundowns <tom@smallridge.com.au>
2019-07-04 20:34:26 +0800

    cleanup test names

--- tests/systems/stage_manager_spec.lua
+++ tests/systems/stage_manager_spec.lua
@@ -34,98 +34,93 @@ end
 _t.fixture(
     "Stage Manager System",
     function(fixture)
+        init_system({})
+        reset_dependecies()
+
         fixture(
-            "Stage Loading Context",
+            "Given no collision data",
             function(fixture)
-                init_system({})
+                -- Arrange
+                reset_dependecies()
+                stage_manager_system:set_collision_world(nil)
+                -- Act & Assert will error
+                _t.expect_error(
+                    fixture,
+                    function()
+                        world_instance:emit("load_stage", "stage_00")
+                    end,
+                    "it throws an error"
+                )
+            end
+        )
+
+        fixture(
+            "Given a map file with no world layer",
+            function(fixture)
+                -- Arrange
+                reset_dependecies()
+                -- Act & Assert will error
+                _t.expect_error(
+                    fixture,
+                    function()
+                        world_instance:emit("load_stage", "stage_00")
+                    end,
+                    "it throws an error"
+                )
+            end
+        )
+
+        fixture(
+            "Given map with valid tiles defined in 'World' layer",
+            function(fixture)
+                -- Arrange
+                reset_dependecies()
+                -- Act
+                world_instance:emit("load_stage", "stage_01")
+
+                -- Assert
+                _t.expect(
+                    fixture,
+                    assert.spy(stage_manager_system.collision_world.add).was.called(4),
+                    "the expected number of tiles were added to the stage_manager table"
+                )
+                _t.expect(
+                    fixture,
+                    #stage_manager_system.tiles == 4,
+                    "the expected number of tiles were added to the collision world"
+                )
+            end
+        )
+
+        fixture(
+            "Given map with valid objects defined in 'Object' layer",
+            function(fixture)
+                -- Arrange
                 reset_dependecies()
 
-                fixture(
-                    "Given unset collision data",
-                    function(fixture)
-                        -- Arrange
-                        reset_dependecies()
-                        stage_manager_system:set_collision_world(nil)
-                        -- Act & Assert will error
-                        _t.expect_error(
-                            fixture,
-                            function()
-                                world_instance:emit("load_stage", "stage_00")
-                            end,
-                            "then it throws an error"
-                        )
-                    end
+                -- Act
+                world_instance:emit("load_stage", "stage_02")
+
+                -- Assert
+                _t.expect(
+                    fixture,
+                    #stage_manager_system.objects == 2,
+                    "the expected number of objects were added to the stage_manager table"
                 )
-
-                fixture(
-                    "Given map with no world layer",
-                    function(fixture)
-                        -- Arrange
-                        reset_dependecies()
-                        -- Act & Assert will error
-                        _t.expect_error(
-                            fixture,
-                            function()
-                                world_instance:emit("load_stage", "stage_00")
-                            end,
-                            "then it throws an error"
-                        )
-                    end
+                _t.expect(
+                    fixture,
+                    world_instance.entities.size == 2,
+                    "the expected number of entities were added to the instance"
                 )
-
-                fixture(
-                    "Given map with valid tiles defined in 'World' layer",
-                    function(fixture)
-                        -- Arrange
-                        reset_dependecies()
-                        -- Act
-                        world_instance:emit("load_stage", "stage_01")
-
-                        -- Assert
-                        _t.expect(
-                            fixture,
-                            assert.spy(stage_manager_system.collision_world.add).was.called(4),
-                            "The expected number of tiles were added to the stage_manager table"
-                        )
-                        _t.expect(
-                            fixture,
-                            #stage_manager_system.tiles == 4,
-                            "The expected number of tiles were added to the collision world"
-                        )
-                    end
+                _t.expect(
+                    fixture,
+                    stage_manager_system.objects[1].type == "static_light_orange",
+                    "the first object has the correct type"
                 )
-
-                fixture(
-                    "Given map with valid objects defined in 'Object' layer",
-                    function(fixture)
-                        -- Arrange
-                        reset_dependecies()
-
-                        -- Act
-                        world_instance:emit("load_stage", "stage_02")
-
-                        -- Assert
-                        _t.expect(
-                            fixture,
-                            #stage_manager_system.objects == 2,
-                            "The expected number of objects were added to the stage_manager table"
-                        )
-                        _t.expect(
-                            fixture,
-                            world_instance.entities.size == 2,
-                            "The expected number of entities were added to the instance"
-                        )
-                        _t.expect(
-                            fixture,
-                            stage_manager_system.objects[1].type == "static_light_orange",
-                            "The first object has the correct type"
-                        )
-                        _t.expect(
-                            fixture,
-                            stage_manager_system.objects[2].type == "static_light_orange",
-                            "The second object has the correct type"
-                        )
-                    end
+                _t.expect(
+                    fixture,
+                    stage_manager_system.objects[2].type == "static_light_orange",
+                    "the second object has the correct type"
                 )
             end
         )
